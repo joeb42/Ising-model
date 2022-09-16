7356d2d930dcc7ae64a84c1ae9fa7af450866443
joeb42 <70596457+joeb42@users.noreply.github.com>
2022-09-16 21:41:21 +0100

    declutter

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,27 @@
 #include <iostream>
 #include <string>
 
+using std::stoi, std::stof;
+
 int main(int argc, char **argv) {
-    int dim = std::stoi(argv[1]);
-    float T = std::stof(argv[2]);
-    int sweeps = std::stoi(argv[3]);
-    int threads = std::stoi(argv[4]);
+    int dim, sweeps, threads;
+    float T;
+    if (argc != 5) {
+        dim = 100;
+        T = 1.1;
+        sweeps = 100;
+        threads = 4;
+    }
+    else {
+        dim = stoi(argv[1]);
+        T = stof(argv[2]);
+        sweeps = stoi(argv[3]);
+        threads = stoi(argv[4]);
+    }
     Lattice2D lattice(dim, T, 4);
     for (int i = 0; i < sweeps; i++) {
         lattice.sweep();
     }
     lattice.write_grid();
     return 0;
-}
-
+} 
